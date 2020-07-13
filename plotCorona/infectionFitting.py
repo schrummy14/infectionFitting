f335adfb28c5a9a623c7ab6d4b2fbd95522b3faa
schrummy14 <schrummy14@gmail.com>
2020-07-12 23:19:04 -0500

    Added code to just plot raw data with no fit

---
+++ plotCorona/infectionFitting.py
@@ -0,0 +1,216 @@
+import os
+import time
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from tqdm import tqdm
+from datetime import datetime
+from odeSolve import odeSolve
+from scipy.optimize import least_squares
+
+def sir(t,x,cS,cR,cD,tS,tR,tD):
+    s = x[0]
+    i = x[1]
+    if s < 0.0:
+        s = 0.0
+    if s > 1.0:
+        s = 1.0
+    if i < 0.0:
+        i = 0.0
+    if i > 1.0:
+        i = 0.0
+
+    dsdt = -cS*np.exp(-tS*t)*s*i
+    dddt =  cD*np.exp(-tD*t)*i
+
+    if dddt < 0.0:
+        dddt = 0.0
+
+    didt = -cR*np.exp( tR*t)*i + cS*np.exp(-tS*t)*s*i - cD*np.exp(-tD*t)*i
+    return np.array([dsdt,didt,dddt])
+
+def movingMean(x,w=5):
+    out = np.cumsum(x)
+    out[w:] = out[w:] - out[:-w]
+    return out[w-1:]/w
+
+def running_mean(x, N=3):
+    out = np.zeros_like(x, dtype=np.float64)
+    dim_len = x.shape[0]
+    for i in range(dim_len):
+        if N%2 == 0:
+            a, b = i - (N-1)//2, i + (N-1)//2 + 2
+        else:
+            a, b = i - (N-1)//2, i + (N-1)//2 + 1
+
+        #cap indices to min and max indices
+        a = max(0, a)
+        b = min(dim_len, b)
+        out[i] = np.mean(x[a:b])
+    return out
+
+def showDailyChange(t,I,D):
+    numDays = 14
+    plt.subplot(121)
+    # dI = np.gradient(I,t)
+    dI = np.zeros(len(I))
+    dI[1:] = I[1:] - I[:-1]
+    dI[dI < 0] = 0
+    plt.plot(t-t[0],dI,'ko',label="Daily Change")
+    mdI = running_mean(dI,numDays)
+    plt.plot(t-t[0],mdI,'k-',linewidth=2.5,label="%i Day Running Average"%numDays)
+    plt.xlabel('Days Since First Death')
+    plt.ylabel('Daily Additional Infected')
+    plt.legend()
+
+    plt.subplot(122)
+    # dD = np.gradient(D,t)
+    dD = np.zeros(len(D))
+    dD[1:] = D[1:] - D[0:-1]
+    dD[dD < 0] = 0
+    plt.plot(t-t[0],dD,'ko',label="Daily Change")
+    mdD = running_mean(dD,numDays)
+    plt.plot(t-t[0],mdD,'k-',linewidth=2.5,label="%i Day Running Average"%numDays)
+    plt.xlabel('Days Since First Death')
+    plt.ylabel('Daily Additional Deaths')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+def getRes(t,X,iV,dV):
+    iVV = np.interp(t,X[:,0],X[:,2])
+    dVV = np.interp(t,X[:,0],X[:,3])
+    res = (iV-iVV)/iV[-1]
+    res = np.concatenate((res,(dV-dVV)/dV[-1]), axis = 0)
+    return res
+
+def getStats(t,X,iV,dV):
+    iVV = np.interp(t,X[:,0],X[:,2])
+    dVV = np.interp(t,X[:,0],X[:,3])
+    Yp = np.concatenate((iVV,dVV), axis = 0)
+    Y = np.concatenate((iV,dV), axis = 0)
+    Ybar = np.mean(Y)
+    res = Yp-Y
+    SSE = np.sum(res*res)
+    SST = np.sum((Y-Ybar)*(Y-Ybar))
+    n = len(Y)
+    p = 6.0
+    R2 = 1.0 - SSE/SST
+    R2adj = 1.0 - SSE*(n-1.0)/(SST*(n-p-1.0))
+    return SSE, SST, R2, R2adj
+
+def sirFit(b,t,iV,dV):
+    fun = lambda t,x: sir(t,x,b[0],b[1],b[2],b[3],b[4],b[5])
+    ode = odeSolve(fun=fun, solver='scipy45')
+    ode.rtol = 1.0e-12
+    ode.atol = 1.0e-14
+    ode.y0 = np.array([1.0-iV[0]-dV[0],iV[0],dV[0]])
+    ode.tspan = [t[0], t[-1]]
+    ode.solve()
+    return 100*getRes(t,ode.sol,iV,dV)
+
+def isZero(t,x):
+    if x[1] < 0.0:
+        return -1.0
+    else:
+        return  1.0
+
+def save(ode, resB, totalPopulation, tVals, iVals, dVals, region, write2file=True):
+    SSE, SST, R2, R2adj = getStats(tVals, ode.sol, iVals, dVals)
+    
+    if write2file:
+        reg = region.replace(' ','_')
+        now = datetime.now()
+        filename = "results/" + reg + now.strftime("_%Y_%m_%d_%H_%M_%S") + ".txt"
+        with open(filename,'w') as f:
+            printWrite(f, "===================================", write2file)
+            printWrite(f, "======== Ending Statistics ========", write2file)
+            printWrite(f, "             Dead: %10d" % int(totalPopulation*ode.sol[-1,3]), write2file)
+            printWrite(f, "       Unaffected: %10d" % int(totalPopulation*ode.sol[-1,1]), write2file)
+            printWrite(f, "   Still Infected: %10d" % int(totalPopulation*ode.sol[-1,2]), write2file)
+            printWrite(f, "", write2file)
+            printWrite(f, "   Coeficients", write2file)
+            printWrite(f, "   cS: %e" % resB[0], write2file)
+            printWrite(f, "   cR: %e" % resB[1], write2file)
+            printWrite(f, "   cD: %e" % resB[2], write2file)
+            printWrite(f, "   tS: %e" % resB[3], write2file)
+            printWrite(f, "   tR: %e" % resB[4], write2file)
+            printWrite(f, "   tD: %e" % resB[5], write2file)
+            printWrite(f,"", write2file)
+            printWrite(f, "   Fit Statistics", write2file)
+            printWrite(f, "     SSE = %e" % SSE, write2file)
+            printWrite(f, "     SST = %e" % SST, write2file)
+            printWrite(f, "      r2 = %e" % R2, write2file)
+            printWrite(f, "   r2adj = %e" % R2adj, write2file)
+            printWrite(f, "===================================", write2file)
+    else:
+        f = None
+        printWrite(f, "===================================", write2file)
+        printWrite(f, "======== Ending Statistics ========", write2file)
+        printWrite(f, "             Dead: %10d" % int(totalPopulation*ode.sol[-1,3]), write2file)
+        printWrite(f, "       Unaffected: %10d" % int(totalPopulation*ode.sol[-1,1]), write2file)
+        printWrite(f, "   Still Infected: %10d" % int(totalPopulation*ode.sol[-1,2]), write2file)
+        printWrite(f, "", write2file)
+        printWrite(f, "   Coeficients", write2file)
+        printWrite(f, "   cS: %e" % resB[0], write2file)
+        printWrite(f, "   cR: %e" % resB[1], write2file)
+        printWrite(f, "   cD: %e" % resB[2], write2file)
+        printWrite(f, "   tS: %e" % resB[3], write2file)
+        printWrite(f, "   tR: %e" % resB[4], write2file)
+        printWrite(f, "   tD: %e" % resB[5], write2file)
+        printWrite(f,"", write2file)
+        printWrite(f, "   Fit Statistics", write2file)
+        printWrite(f, "     SSE = %e" % SSE, write2file)
+        printWrite(f, "     SST = %e" % SST, write2file)
+        printWrite(f, "      r2 = %e" % R2, write2file)
+        printWrite(f, "   r2adj = %e" % R2adj, write2file)
+        printWrite(f, "===================================", write2file)
+
+def printWrite(f, curStr, write2file):
+    print(curStr)
+    if write2file:
+        f.write(curStr + "\n")
+
+def runOne(fun, lb, ub, doeVals, numRuns):
+    minCost = np.inf
+    for k in tqdm(range(numRuns)):
+        b0 = doeVals[k,:]*(ub-lb) + lb
+        curRes = least_squares(
+            fun=fun, 
+            x0=b0,
+            bounds=(lb,ub),
+            verbose=0,
+            loss='linear',
+            xtol=5.0e-16,
+            ftol=5.0e-16
+        )
+        b = curRes.x
+        cost = curRes.cost
+
+        if cost < minCost:
+            print("   Min Cost =", cost)
+            minCost = cost
+            resB = b
+    return resB
+
+
+def getData(mdy):
+    fName = '..' + os.sep + 'rawData' + os.sep + mdy
+    try:
+        df = pd.read_csv(fName,parse_dates=[2])
+    except:
+        print("Could not find file:", fName)
+        print("Atempting to download the file...")
+        baseURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+        url = baseURL + mdy
+        try:
+            df = pd.read_csv(url,parse_dates=[2])
+            df.to_csv(fName)
+        except:
+            print("Could not download file")
+            df = None
+    return df
---
+++ plotCorona/plotCorona.py
@@ -0,0 +1,388 @@
+
+import sys
+import pyDOE
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import infectionFitting as infect
+
+infectionName = "coronavirus"
+
+startDate = dt.date(2020,3,1)
+endDate = dt.datetime.now() + dt.timedelta(days=-1)
+endDate = dt.date(endDate.year,endDate.month,endDate.day)
+baseURL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'
+
+def getData(date):
+    mdy = getDateStr(date)
+    df = infect.getData(mdy)
+    return df
+
+def getDateStr(date):
+    m = date.month
+    d = date.day
+    if m < 10:
+        mdy = "0%s-"%m
+    else: 
+        mdy = "%s-"%m
+    if d < 10:
+        mdy += "0%s-2020.csv"%d
+    else:
+        mdy += "%s-2020.csv"%d
+    return mdy
+
+
+def printGrowthFactor(I):
+    deltaI = I[1:]-I[0:-1]
+    print(deltaI[1:]/deltaI[0:-1])
+
+
+def createTimeDataCountry():
+    region = sys.argv[1]
+    infected = []
+    deaths = []
+    days = []
+
+    posRegions = getRegionsCountry()
+    if region not in posRegions:
+        print("Region: %s, is not a posible region..." % region)
+        print("Possible Regions:\n")
+        print(posRegions)
+        exit()
+
+    k = 0
+    while True:
+        curDate = startDate + dt.timedelta(days=k)
+        k+=1
+        if curDate > endDate:
+            break
+        df = getData(curDate)
+        cr = 'Country/Region'
+        try:
+            df[cr]
+        except:
+            cr = 'Country_Region'
+        try:
+            if df is None:
+                continue
+            if region == "all":
+                break
+            elif region == "world":
+                infected.append(np.sum(df['Confirmed']))
+                deaths.append(np.sum(df['Deaths']))
+                days.append(k)
+            else:
+                if region == 'China':
+                    gg = df.loc[df[cr]=='China']
+                    if len(gg) == 0:
+                        gg = gg = df.loc[df[cr]=='Mainland China']
+                else:
+                    gg = df.loc[df[cr]==region]
+                numInfected = np.sum(gg['Confirmed'])
+                numDeaths = np.sum(gg['Deaths'])
+                if numInfected*numDeaths > 0:
+                    infected.append(np.sum(gg['Confirmed']))
+                    deaths.append(np.sum(gg['Deaths']))
+                    days.append(k)
+        except:
+            print(df)
+    infected = np.array(infected)
+    deaths = np.array(deaths)
+    days = np.array(days)
+
+    ids = []
+    temp = np.where(infected>0)
+    ids.append(temp[0])
+    temp = np.where(deaths>0)
+    ids.append(temp[0])
+    minIDlen = 1.0e10
+    for idc in ids:
+        if len(idc) < minIDlen:
+            minIDlen = len(idc)
+            minID = idc
+
+    if minIDlen > 3:
+        return infected[minID],deaths[minID],days[minID]
+    else:
+        print("Not enough data... Exiting...")
+        daysAll = np.array(daysAll)
+        infectedAll = np.array(infectedAll)
+        deathsAll = np.array(deathsAll)
+        printFlag = False
+        print('Days, Infected, Deaths')
+        for k in range(len(infectedAll)):
+            if infectedAll[k] > 0:
+                printFlag = True
+            if printFlag:
+                print("%d, %d, %d" % (daysAll[k],infectedAll[k],deathsAll[k]))
+                # print(daysAll[k],', ',infectedAll[k],', ',deathsAll[k])
+        infect.showDailyChange(daysAll,infectedAll,deathsAll)
+        exit()
+
+def getRegionsCountry():
+    date = endDate + dt.timedelta(days=-3)
+    mdy = getDateStr(date)
+    url = baseURL + mdy
+    df = pd.read_csv(url,parse_dates=[2])
+    cr = 'Country/Region'
+    try:
+        df[cr]
+    except:
+        cr = 'Country_Region'
+    regions = df[cr].unique()
+    return regions
+
+def createTimeDataState():
+    
+    
+    infected = []
+    deaths = []
+    days = []
+    region = sys.argv[2]
+
+    posRegions = getRegionsState()
+    if region not in posRegions:
+        print("Region: %s, is not a posible region..." % region)
+        print("Possible Regions:\n")
+        print(posRegions)
+        exit()
+    
+
+    k = 0
+    gotData = False
+    while True:
+        curDate = startDate + dt.timedelta(days=k)
+        k+=1
+        if curDate > endDate:
+            break
+        df = getData(curDate)
+        cr = 'Province_State'
+        try:
+            df[cr]
+        except:
+            cr = 'Province_State'
+        try:
+            if df is None:
+                continue
+            if region == "all":
+                break
+            elif region == "US":
+                infected.append(np.sum(df['Confirmed']))
+                deaths.append(np.sum(df['Deaths']))
+                days.append(k)
+            else:
+                gg = df.loc[df[cr]==region]
+                numInfected = np.sum(gg['Confirmed'])
+                numDeaths = np.sum(gg['Deaths'])
+                if numInfected*numDeaths > 0:
+                    infected.append(np.sum(gg['Confirmed']))
+                    deaths.append(np.sum(gg['Deaths']))
+                    days.append(k)
+            gotData = True
+        except:
+            if gotData:
+                print(df)
+    infected = np.array(infected)
+    deaths = np.array(deaths)
+    days = np.array(days)
+
+    ids = []
+    temp = np.where(infected>0)
+    ids.append(temp[0])
+    temp = np.where(deaths>0)
+    ids.append(temp[0])
+    minIDlen = 1.0e10
+    for idc in ids:
+        if len(idc) < minIDlen:
+            minIDlen = len(idc)
+            minID = idc
+
+    if minIDlen > 3:
+        return infected[minID],deaths[minID],days[minID]
+    else:
+        print("Not enough data... Exiting...")
+        daysAll = np.array(daysAll)
+        infectedAll = np.array(infectedAll)
+        deathsAll = np.array(deathsAll)
+        printFlag = False
+        print('Days, Infected, Deaths')
+        for k in range(len(infectedAll)):
+            if infectedAll[k] > 0:
+                printFlag = True
+            if printFlag:
+                print("%d, %d, %d" % (daysAll[k],infectedAll[k],deathsAll[k]))
+                # print(daysAll[k],', ',infectedAll[k],', ',deathsAll[k])
+        infect.showDailyChange(daysAll,infectedAll,deathsAll)
+        exit()
+
+def getRegionsState():
+    date = endDate + dt.timedelta(days=-3)
+    mdy = getDateStr(date)
+    url = baseURL + mdy
+    try:
+        df = pd.read_csv(url,parse_dates=[2])
+    except:
+        print(url)
+        df = pd.read_csv(url)
+        print(df)
+        exit()
+    cr = 'Country/Region'
+    try:
+        df[cr]
+    except:
+        cr = 'Country_Region'
+    df = df[df[cr]==sys.argv[1]]
+    regions = df['Province_State'].unique()
+    return regions
+
+def createTimeDataCounty():
+    infected = []
+    deaths = []
+    days = []
+
+    infectedAll = []
+    deathsAll = []
+    daysAll = []
+
+    state = sys.argv[2]
+    region = sys.argv[3]
+
+    if not (region == 'all'):
+        posRegions = getRegionsCounty()
+        if region not in posRegions:
+            print("Region: %s, is not a posible region..." % region)
+            print("Possible Regions:\n")
+            print(posRegions)
+            exit()
+
+    k = 0
+    gotData = False
+    while True:
+        curDate = startDate + dt.timedelta(days=k)
+        k+=1
+        if curDate > endDate:
+            break
+        df = getData(curDate)
+        cr = 'Province_State'
+        try:
+            df[cr]
+            df = df[df[cr]==state]
+            cr = 'Admin2'
+        except:
+            cr = 'Province_State'
+        try:
+            if df is None:
+                continue
+            if region == "all":
+                infected.append(np.sum(df['Confirmed']))
+                deaths.append(np.sum(df['Deaths']))
+                days.append(k)
+            else:
+                gg = df.loc[df[cr]==region]
+                numInfected = np.sum(gg['Confirmed'])
+                numDeaths = np.sum(gg['Deaths'])
+                infectedAll.append(numInfected)
+                deathsAll.append(numDeaths)
+                daysAll.append(k)
+                if numInfected*numDeaths > 0:
+                    infected.append(np.sum(gg['Confirmed']))
+                    deaths.append(np.sum(gg['Deaths']))
+                    days.append(k)
+            gotData = True
+        except:
+            if gotData:
+                print(df)
+    
+    infected = np.array(infected)
+    deaths = np.array(deaths)
+    days = np.array(days)
+
+    ids = []
+    temp = np.where(infected>0)
+    ids.append(temp[0])
+    temp = np.where(deaths>0)
+    ids.append(temp[0])
+    minIDlen = 1.0e10
+    for idc in ids:
+        if len(idc) < minIDlen:
+            minIDlen = len(idc)
+            minID = idc
+
+    if minIDlen > 3:
+        return infected[minID],deaths[minID],days[minID]
+    else:
+        print("Not enough data... Exiting...")
+        daysAll = np.array(daysAll)
+        infectedAll = np.array(infectedAll)
+        deathsAll = np.array(deathsAll)
+        printFlag = False
+        print('Days, Infected, Deaths')
+        for k in range(len(infectedAll)):
+            if infectedAll[k] > 0:
+                printFlag = True
+            if printFlag:
+                print("%d, %d, %d" % (daysAll[k],infectedAll[k],deathsAll[k]))
+                # print(daysAll[k],', ',infectedAll[k],', ',deathsAll[k])
+        infect.showDailyChange(daysAll,infectedAll,deathsAll)
+        exit()
+
+
+def getRegionsCounty():
+    if not sys.argv[1] == 'US':
+        print("Currently only works with the US")
+        exit()
+    date = endDate + dt.timedelta(days=-3)
+    mdy = getDateStr(date)
+    url = baseURL + mdy
+    try:
+        df = pd.read_csv(url,parse_dates=[2])
+    except:
+        print(url)
+        df = pd.read_csv(url)
+        print(df)
+        exit()
+    cr = 'Province_State'
+    try:
+        df[cr]
+    except:
+        cr = 'Province_State'
+    df = df[df[cr]==sys.argv[2]]
+    cr = 'Admin2'
+    regions = df[cr].unique()
+    return regions
+
+def printRawData(I,D,T):
+    data = np.transpose(np.array([T,I,D]))
+    print("Days, Infected, Deaths")
+    print(data)
+
+if __name__ == "__main__":
+
+    numArg = len(sys.argv)
+    if numArg == 1:
+        print("Error: Usage-> plotCorona.py [country] [state {optional}] [county {optional}]")
+        print("               plotCorona.py getRegions [country {optional}] [state {optional}]")
+        exit()
+    
+    if sys.argv[1] == 'getRegions':
+        getRegions()
+        exit()
+
+    country = sys.argv[1]
+    if numArg == 2:
+        I,D,T = createTimeDataCountry()
+    elif numArg == 3:
+        I,D,T = createTimeDataState()
+    else:
+        I,D,T = createTimeDataCounty()
+        
+    
+    print("\nRaw Data")
+    printRawData(I,D,T)
+    print("\nInfection Growth Factor")
+    printGrowthFactor(I)
+    print("\nDeath Growth Factor")
+    printGrowthFactor(D)
+
+    infect.showDailyChange(T,I,D)
